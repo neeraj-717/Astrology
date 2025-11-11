77aece96573f00899f628341fb45efd0bc0dd448
MAC <mac@MACs-MBP.bbrouter>
2025-11-11 14:54:57 +0530

    new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors({
   origin: [
-    "https://astrologyf.vercel.app",  
+    "https://astrology-new-seven.vercel.app/",  
     "http://localhost:5173"                    
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
